2b7e6999fabb4bef3c2b276b979f38238aa92bc9
Jelani Woods <jelani@firstdraft.com>
2023-02-02 14:10:00 -0600

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@
 #  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
