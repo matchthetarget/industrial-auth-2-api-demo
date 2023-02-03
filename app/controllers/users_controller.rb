55aea9b4fcda687000819be7b454e156f9ce416b
Jelani Woods <jelani@firstdraft.com>
2023-02-03 11:29:33 -0600

    Fix issue with authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     def set_user
       if json_request?
-        @user = tokenized_user
+        p @user = tokenized_user
       elsif params[:username]
         @user = User.find_by!(username: params.fetch(:username))
       else
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def must_be_owner_to_view
       if json_request? && @user.blank?
         render json: { status: 401, message: "You're not authorized for that." }
-      elsif current_user != @user
+      elsif current_user != @user && !json_request?
         redirect_back fallback_location: root_url, alert: "You're not authorized for that."
       end
     end
